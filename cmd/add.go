a955e4a163c51596caa7045e26b28f2e8b698d56
anakinsonone <akkhi.sonone@gmail.com>
2024-09-15 16:55:06 +0530

    add a new flag for setting a due date

--- cmd/add.go
+++ cmd/add.go
@@ -1,6 +1,16 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"time"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	minutes int
+	hours   int
+	days    int
+)
 
 var addCmd = &cobra.Command{
 	Use:   "add",
@@ -8,7 +18,18 @@ var addCmd = &cobra.Command{
 	Short: "Add todo.",
 	Long:  "Add a todo to the database.",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		err := Add(args[0])
+		task := args[0]
+		var dueDate *time.Time
+
+		if minutes > 0 || hours > 0 || days > 0 {
+			due := time.Now().
+				Add(time.Duration(minutes) * time.Minute).
+				Add(time.Duration(hours) * time.Hour).
+				Add(time.Duration(days) * 24 * time.Hour)
+			dueDate = &due
+		}
+
+		err := Add(task, dueDate)
 		if err != nil {
 			return err
 		}
@@ -19,4 +40,9 @@ var addCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(addCmd)
+
+	addCmd.Flags().
+		IntVarP(&minutes, "minutes", "m", 0, "Due in specified number of minutes from now.")
+	addCmd.Flags().IntVarP(&hours, "hours", "r", 0, "Due in specified number of hours from now.")
+	addCmd.Flags().IntVarP(&days, "days", "d", 0, "Due in specified number of days from now.")
 }
